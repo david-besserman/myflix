852a867bf9ef6b1ff997d8154394d6f606724899
David Besserman <david.besserman@gmail.com>
2015-11-12 09:49:24 +0100

    Pre-course assignments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "falmily guy", small_cover_url: "public/tmp/family_guy.jpg", large_cover_url: "public/tmp/family_guy.jpg", description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.")
+Video.create(title: "futurama", small_cover_url: "public/tmp/futurama.jpg", large_cover_url: "public/tmp/futurama.jpg", description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.")
+Video.create(title: "monk", small_cover_url: "public/tmp/monk.jpg", large_cover_url: "public/tmp/monk_large", description:"Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.")
+
+Video.create(title: "south park", small_cover_url: "public/tmp/south park.jpg", large_cover_url: "public/tmp/south park.jpg", description:"Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.")
+
+Category.create(name: "TV Comedies")
+Category.create(name: "TV Dramas")
