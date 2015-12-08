9ff23650b87aa051325d3d8d5439e86b04ee81d1
David Besserman <david.besserman@gmail.com>
2015-12-08 12:21:36 +0100

    controller test for authentication and ability to review videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 comedies = Category.create!(name: "TV Comedies")
 dramas = Category.create!(name: "TV Dramas")
 
-Video.create!(title: "falmily guy", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/family_guy.jpg", description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.", category: dramas)
+fam_guy = Video.create!(title: "falmily guy", small_cover_url: "/tmp/family_guy.jpg", large_cover_url: "/tmp/family_guy.jpg", description: "In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.", category: dramas)
 Video.create!(title: "monk", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large", description:"Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.", category: dramas)
 
 Video.create!(title: "south park", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park.jpg", description:"Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.",category: comedies)
@@ -21,4 +21,7 @@ Video.create!(title: "futurama", small_cover_url: "/tmp/futurama.jpg", large_cov
 Video.create!(title: "futurama", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", category: comedies)
 Video.create!(title: "futurama", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", description:"Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", category: comedies)
 
-User.create(email: 'david@test.com', password: 'password', full_name: 'David B')
+david = User.create(email: 'david@test.com', password: 'password', full_name: 'David B')
+
+Review.create(user: david, video: fam_guy, content: 'love that show', rating: 4)
+Review.create(user: david, video: fam_guy, content: 'hate that show', rating: 2)
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,32 +2,49 @@ require 'spec_helper'
 
 describe VideosController do
   describe 'GET show' do
-    it 'sets @video for authenticated users' do
-      session[:user_id] = Fabricate(:user).id
-      video = Fabricate(:video)
-      get :show, id: video.id
-      expect(assigns(:video)).to eq(video)
+    let(:video) { Fabricate(:video) }
+
+    context 'authenticated user' do
+      before do
+        session[:user_id] = Fabricate(:user).id
+      end
+      it 'sets @video' do
+        get :show, id: video.id
+        expect(assigns(:video)).to eq(video)
+      end
+      
+      it 'sets @reviews' do
+        review1 = Fabricate(:review, video: video)
+        review2 = Fabricate(:review, video: video)
+        get :show, id: video.id
+        assigns(:reviews).should =~ [review1, review2]
+      end
     end
 
-    it 'redirects to the sign in page for unauthenticated users' do
-      video = Fabricate(:video)
-      get :show, id: video.id
-      expect(response).to redirect_to sign_in_path
-    end 
+    context 'unauthenticated user' do
+      it 'redirects to the sign in page' do
+        get :show, id: video.id
+        expect(response).to redirect_to sign_in_path
+      end 
+    end
   end
 
   describe '#POST search' do
-    it 'sets @results for authenticated users' do
-      session[:user_id] = Fabricate(:user).id
-      futurama = Fabricate(:video, title: 'futurama')
-      post :search, search_term: 'rama'
-      expect(assigns(:results)).to eq([futurama])
+    let(:futurama) { Fabricate(:video, title: 'futurama' )}
+
+    context 'authenticated user' do
+      it 'sets @results' do
+        session[:user_id] = Fabricate(:user).id
+        post :search, search_term: 'rama'
+        expect(assigns(:results)).to eq([futurama])
+      end
     end
 
-    it 'redirects to the sign in page for unauthenticated users' do
-      futurama = Fabricate(:video, title: 'futurama')
-      post :search, search_term: 'rama'
-      expect(assigns(:results)).to redirect_to sign_in_path
+    context 'unauthenticated user' do
+      it 'redirects to the sign in page' do
+        post :search, search_term: 'rama'
+        expect(assigns(:results)).to redirect_to sign_in_path
+      end
     end
   end
 end
