2e74f96edd735c258fda1c62b114e37c2e4f19f6
David Besserman <david.besserman@gmail.com>
2016-01-14 17:12:07 +0100

    User adds a video to my queue

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,32 @@
 Myflix::Application.routes.draw do
   root to: 'pages#front'
    
+  # videos controller
   get '/home', to: 'videos#index'
-  get 'ui(/:action)', controller: 'ui'
-  get '/register', to: 'users#new'
-  get 'sign_in', to: 'sessions#new'
-  get 'sign_out', to: 'sessions#destroy'
-  
-  get '/my_queue', to: 'queue_items#index'
-
-  resources :users, only: [:create]
-  resources :sessions, only: [:create]
-  resources :categories, only: [:show]
   resources :videos, only: [:show] do
     collection do 
       post :search, to: 'videos#search'
     end
     resources :reviews, only: [:create]
   end
+  
+  # users controller
+  get '/register', to: 'users#new'
+  resources :users, only: [:create]
+  
+  # sessions controller
+  get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
+  resources :sessions, only: [:create]
 
+  # queue items controller
+  get '/my_queue', to: 'queue_items#index'
+  resources :queue_items, only: [:create]
 
+  
+  # other controllers
+  get 'ui(/:action)', controller: 'ui'
+  get 'haml_test', to: 'pages#haml_test' #TODO: delelte
 
-
-  get 'haml_test', to: 'pages#haml_test'
+  resources :categories, only: [:show]
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -10,9 +10,72 @@ describe QueueItemsController do
       get :index
       expect(assigns(:queue_items)).to match_array([queue_item1, queue_item2])
     end
+
     it "redirects to the sign-in page for unaunthenticated users" do
       get :index
       expect(response).to redirect_to sign_in_path
+    end
+  end
+
+  describe 'POST create' do
+    context 'with authenticated user' do
+      it 'redirects to the my_queue page' do
+        session[:user_id] = Fabricate(:user).id
+        vid = Fabricate(:video)
+        post :create, video_id: vid.id
+        expect(response).to redirect_to my_queue_path
+      end
+      
+      it 'creates a queue item' do
+        session[:user_id] = Fabricate(:user).id
+        vid = Fabricate(:video)
+        post :create, video_id: vid.id
+        expect(QueueItem.count).to eq(1) 
+      end
+
+      it 'creates a queue item that is associated with the video' do
+        session[:user_id] = Fabricate(:user).id
+        vid = Fabricate(:video)
+        post :create, video_id: vid.id
+        expect(QueueItem.first.video).to eq(vid) 
+      end
+
+      it 'creates a queue item that is associated with the signed in user' do
+        alice = Fabricate(:user)
+        session[:user_id]  = alice.id
+        vid = Fabricate(:video)
+        post :create, video_id: vid.id
+        expect(QueueItem.first.user).to eq(alice) 
+      end
+
+      it 'puts the video as the last one in the queue' do
+        alice = Fabricate(:user)
+        session[:user_id]  = alice.id
+        monk = Fabricate(:video)
+        Fabricate(:queue_item, video: monk, user: alice)
+        south_park = Fabricate(:video)
+        post :create, video_id: south_park.id
+        south_park_queue_item = QueueItem.where(video_id: south_park.id, user_id: alice.id).first
+        expect(south_park_queue_item.position).to eq(2)
+      end
+      
+      context 'with the video already in the queue' do
+        it 'does not add the video to the queue' do
+          alice = Fabricate(:user)
+          session[:user_id]  = alice.id
+          monk = Fabricate(:video)
+          Fabricate(:queue_item, video: monk, user: alice)
+          post :create, video_id: monk.id
+          expect(alice.queue_items.count).to eq(1)
+        end
+      end
+    end
+
+    context 'with unauthenticated user' do
+      it 'redirects to the sign in page' do
+        post :create, video_id: 3
+        expect(response).to redirect_to sign_in_path
+      end
     end
   end
 end
