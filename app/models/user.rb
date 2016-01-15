08ea485b1966b4e346423eee79479e9d6843352e
David Besserman <david.besserman@gmail.com>
2016-01-15 17:51:40 +0100

    Refacoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,16 @@ class User < ActiveRecord::Base
 
   validates_presence_of :email, :password, :full_name
   validates_uniqueness_of :email
+
+  def new_queue_item_position
+    queue_items.count + 1
+  end
+  
+  def queued_video?(video)
+    queue_items.map(&:video).include?(video)
+  end
+
+  def owner?(queue_item)
+    queue_items.include?(queue_item)
+  end
 end
